81514a825745d33d7bf3ace53355d219d3aa9289
rynxu2 <dobaolong209@gmail.com>
2024-03-15 12:28:07 +0700

    better

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import flask
+from flask import Flask
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.get('/')
-def home():
-    return flask.jsonify({'hello'})
+@app.route('/')
+def hello():
+    return '<h1>Xin chào, đây là ứng dụng Flask cơ bản!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
